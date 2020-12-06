afee7041fae8b8618882e4c79ecc121ae2d4d19d
jason7531 <54910136+jason7531@users.noreply.github.com>
2020-12-06 20:13:59 +0530

    Added more templates

--- models/quiz.js
+++ models/quiz.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const quizSchema = new mongoose.Schema({
+  name: { type: String },
   createdAt: { type: Date, expires: 3600, default: Date.now },
   content: [{ type: mongoose.Schema.Types.ObjectId, ref: "Question" }],
   slot: { type: String },
