58c81f3f5aab09e4e31fefa317360db786ab150f
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-12-06 11:30:16 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,6 @@ router.post("/register", async (req, res) => {
     process.env.TOKEN_SECRET
   );
   const user = new User({
-    name: req.body.name,
     email: req.body.email,
     password: hashedPassword,
   });
