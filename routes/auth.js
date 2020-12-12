809b5eceb4ed8b7e7254657bad9a593f738c6ee3
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-12-12 21:07:42 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -55,46 +55,10 @@ router.post("/login", async (req, res) => {
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
   const role = user.role;
   user.password = undefined;
-  // res.render("adminUI/admin", {
-  //   token: token,
-  //   role: role,
-  //   user: user,
-  // });
   res.cookie("token", token);
   res.redirect("/api/admin/dashboard/");
 });
-// router.post("/app/login/", async (req, res) => {
-//   const { error } = loginValidation(req.body);
-//   if (error) return res.status(400).send(error.details[0].message);
 
-//   // const user = await
-//   User.findOne({ email: req.body.email }).exec((err, user) => {
-//     if (err || !user) {
-//       const salt = bcrypt.genSalt(10);
-//       const hashedPassword = bcrypt.hash(req.body.password, salt);
-
-//       var user = new User({
-//         name: req.body.name,
-//         email: req.body.email,
-//         password: hashedPassword,
-//       });
-//       const token = jwt.sign(
-//         { _id: user._id, name: user.name },
-//         process.env.TOKEN_SECRET
-//       );
-//       try {
-//         user = user.save();
-//         res.json(user);
-//       } catch (error) {
-//         res.status(400).send(error);
-//       }
-//     } else {
-//       const validPass = bcrypt.compare(req.body.password, user.password);
-//       if (!validPass) return res.status(400).send("Invalid Password");
-//       res.json(user);
-//     }
-//   });
-// });
 router.post("/app/login/", async (req, res) => {
   User.findOne({ email: req.body.email }).exec(async(err, user) => {
     if (err || !user) {
