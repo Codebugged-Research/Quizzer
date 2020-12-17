31247d7be2ba1ac36301c4ac4c51d9f728a0c4ca
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-12-17 23:02:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ app.post("/api/payment/verify", (req, res) => {
   res.send(response);
 });
 
-app.listen(3000, () => console.log("Server started"));
-// const httpServer = http.createServer(app);
-// httpServer.listen(80, () => {
-//   console.log("HTTP Server running on port 80");
-// });
+// app.listen(3000, () => console.log("Server started"));
+const httpServer = http.createServer(app);
+httpServer.listen(80, () => {
+  console.log("HTTP Server running on port 80");
+});
