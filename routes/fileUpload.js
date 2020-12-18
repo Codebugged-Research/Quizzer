97b1cf09ea69f84289c9e1c3f6f161721574c304
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-12-18 13:45:59 +0530

    upload

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -2,14 +2,14 @@ const fileRouter = require("express").Router();
 const verify = require("./verifyToken");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-
+const Card = require("../models/card");
 const aws = require("aws-sdk");
 aws.config.update({
   accessKeyId: "AKHJVLMXHVD6T5YWQ4PF",
   secretAccessKey: "dc5BXjf/zC1ryGMC4TzxUnQLYsJWPIjQS73kHJx+yf0",
   region: "ams3",
 });
-const uploadEndPoint = new aws.Endpoint("ams3.digitaloceanspaces.com");
+const uploadEndPoint = new aws.Endpoint("ams3.digitaloceanspaces.com/banner");
 const uploads3 = new aws.S3({
   endpoint: uploadEndPoint,
 });
@@ -17,8 +17,17 @@ const uploads3 = new aws.S3({
 fileRouter.get("/upload", async (req, res) => {
   res.render("adminUI/fileUpload");
 });
-
+fileRouter.get("/allCards", (req, res) => {
+  Card.find().exec((err, cards) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.json(cards);
+    }
+  });
+});
 fileRouter.post("/uploadfile", (req, res) => {
+  console.log(req.body);
   const upload = multer({
     storage: multerS3({
       s3: uploads3,
@@ -26,6 +35,11 @@ fileRouter.post("/uploadfile", (req, res) => {
       acl: "public-read",
       key: function (req, file, cb) {
         cb(null, file.originalname);
+        var url =
+          "https://quizaddabox.ams3.digitaloceanspaces.com/banner/" +
+          file.originalname;
+        card = new Card({ imageURL: url });
+        card.save();
       },
     }),
   }).array("upload", 1);
