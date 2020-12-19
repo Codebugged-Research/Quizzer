798101ce48a24b413c30ea704ee6b9a683c639fd
jason7531 <54910136+jason7531@users.noreply.github.com>
2020-12-19 21:02:15 +0530

    Updated feed router

--- routes/feedRouter.js
+++ routes/feedRouter.js
@@ -4,15 +4,25 @@ const File = require("../models/file");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const aws = require("aws-sdk");
+feedRouter.get("/", (req, res) => {
+  File.find().exec((err, files) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.render("adminUI/allFiles", { files: files });
+    }
+  });
+});
 feedRouter.get("/upload", (req, res) => {
   res.render("adminUI/feedUpload");
 });
+
 feedRouter.delete("/:id", verify, async (req, res) => {
   await File.findByIdAndRemove(req.params.id, function (err) {
     if (err) {
       console.log("PROBLEM!");
     } else {
-      res.redirect("/feed/upload");
+      res.redirect("/feed");
     }
   });
 });
