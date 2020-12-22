194737b019bb63dbfb9017c58a22b3e0ac30e3bf
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-12-23 00:34:51 +0530

    Update response.js

--- routes/response.js
+++ routes/response.js
@@ -44,9 +44,9 @@ responseRouter.get("/UserDate/:id", async (req, res) => {
   var today = new Date();
   console.log(today);
   console.log(today.toLocaleString("en-US", {timeZone: "Asia/Kolkata"})); 
-  // // console.log(today.getFullYear());
-  // // console.log(today.getMonth());
-  // // console.log(today.getDate());
+  console.log(today.getHours());
+  console.log(today.getMinutes());
+  console.log(today.getSeconds());
   // res.end();
   // req.json({s:"S"});
   // var today = Date.now();
