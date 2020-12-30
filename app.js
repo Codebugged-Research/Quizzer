fe71ec5ed31bc48f67348dba2a68cb247f8dbb1d
jason7531 <54910136+jason7531@users.noreply.github.com>
2020-12-30 13:40:41 +0530

    removed payment routes

--- app.js
+++ app.js
@@ -74,35 +74,35 @@ app.use("/file", fileRoute);
 app.use("/feed", feedRoute);
 app.use("/offer", offerRoute);
 // Payment Routes
-app.get("/payments", (req, res) => {
-  res.render("adminUI/payment", { key: process.env.KEY_ID });
-});
-app.post("/api/payment/order", (req, res) => {
-  params = req.body;
-  instance.orders
-    .create(params)
-    .then((data) => {
-      res.send({ sub: data, status: "success" });
-    })
-    .catch((error) => {
-      res.send({ sub: error, status: "failed" });
-    });
-});
+// app.get("/payments", (req, res) => {
+//   res.render("adminUI/payment", { key: process.env.KEY_ID });
+// });
+// app.post("/api/payment/order", (req, res) => {
+//   params = req.body;
+//   instance.orders
+//     .create(params)
+//     .then((data) => {
+//       res.send({ sub: data, status: "success" });
+//     })
+//     .catch((error) => {
+//       res.send({ sub: error, status: "failed" });
+//     });
+// });
 
-app.post("/api/payment/verify", (req, res) => {
-  body = req.body.razorpay_order_id + "|" + req.body.razorpay_payment_id;
-  var crypto = require("crypto");
-  var expectedSignature = crypto
-    .createHmac("sha256", process.env.KEY_SECRET)
-    .update(body.toString())
-    .digest("hex");
-  // console.log("sig" + req.body.razorpay_signature);
-  // console.log("sig" + expectedSignature);
-  var response = { status: "failure" };
-  if (expectedSignature === req.body.razorpay_signature)
-    response = { status: "success" };
-  res.send(response);
-});
+// app.post("/api/payment/verify", (req, res) => {
+//   body = req.body.razorpay_order_id + "|" + req.body.razorpay_payment_id;
+//   var crypto = require("crypto");
+//   var expectedSignature = crypto
+//     .createHmac("sha256", process.env.KEY_SECRET)
+//     .update(body.toString())
+//     .digest("hex");
+// console.log("sig" + req.body.razorpay_signature);
+// console.log("sig" + expectedSignature);
+//   var response = { status: "failure" };
+//   if (expectedSignature === req.body.razorpay_signature)
+//     response = { status: "success" };
+//   res.send(response);
+// });
 
 app.listen(3000, () => console.log("Server started"));
 // const httpServer = http.createServer(app);
