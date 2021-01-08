9171b2ed46ee6aa20bdee559a8d3b00ab836a24b
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-01-09 00:22:20 +0530

    up

--- routes/response.js
+++ routes/response.js
@@ -42,6 +42,12 @@ responseRouter.get("/getByUser/:id", async (req, res) => {
 //Get responses by user id and date
 responseRouter.get("/UserDate/:id", async (req, res) => {
   var today = new Date();
+  console.log(today.getFullYear(),
+  today.getMonth(),
+  today.getDate(),
+  5,
+  30,
+  0);
   Response.find({
     user: req.params.id,
     createdAt: {
